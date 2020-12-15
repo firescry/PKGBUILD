362a6484bb38c9314fe1a02521788e263b392d3c
firescry <49809580+firescry@users.noreply.github.com>
2020-12-15 19:05:44 +0100

    Create 'builder' user

--- tools/build.sh
+++ tools/build.sh
@@ -6,7 +6,7 @@ for PKGBUILD in $(find ${GITHUB_WORKSPACE} -name PKGBUILD -type f | sort); do
     echo "Processing ${PKGBUILD}"
     if [[ ${PKGBUILD} =~ rtl8822bu ]]; then
         cd $(dirname ${PKGBUILD})
-        makepkg -C -s --noconfirm
+        sudo -u builder makepkg -C -s --noconfirm
     else
         echo Skipping
     fi
